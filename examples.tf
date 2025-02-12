6af33849530b958b0f2a585cce41c2b4bc16d35d
Angel Rivera <2404388+punkdata@users.noreply.github.com>
2025-02-12 16:22:52 -0500

    Update examples.tf

--- examples.tf
+++ examples.tf
@@ -12,30 +12,22 @@ provider "aws" {
   region = "us-east-1"
 }
 
-variable "account_id" {
-    default = "9000231757479"
-}
-
-data "aws_dynamodb_table_item" "test_aft_request"{
-    table_name="test_aft_request"
+data "aws_dynamodb_table_item" "aft_request_metadata"{
+    table_name="aft_request_metadata"
 
     projection_expression = "email"
-    key                   = <<KEY
-    {
-        "id": {"S": "${var.account_id}"}
-    }
-    KEY
+    key = jsonencode({
+        id = { S = var.account_id }
+    })
 }
 
-data "aws_dynamodb_table_item" "test_aft_request_metadata"{
-    table_name="test_aft_request_metadata"
+data "aws_dynamodb_table_item" "aft_request"{
+    table_name="aft_request"
 
     projection_expression = "custom_fields"
-    key                   = <<KEY
-    {
-        "id": {"S": "${jsondecode(data.aws_dynamodb_table_item.test_aft_request.item)["email"].S}"}
-    }
-    KEY
+    key = jsonencode({
+        id = { S = jsondecode(data.aws_dynamodb_table_item.aft_request_metadata.item)["email"].S }
+      })
 }
 
 output "var-account-id" {
@@ -43,12 +35,12 @@ output "var-account-id" {
 }
 
 output "aft_req_email" {
-    value=jsondecode(data.aws_dynamodb_table_item.test_aft_request.item)["email"].S
+    value=jsondecode(data.aws_dynamodb_table_item.aft_request_metadata.item)["email"].S
   
 }
 
 output "aft_req_custom_fields" {
 
-    value = jsondecode(jsondecode(data.aws_dynamodb_table_item.test_aft_request_metadata.item)["custom_fields"].S)["alias"]
+    value = jsondecode(jsondecode(data.aws_dynamodb_table_item.aft_request.item)["custom_fields"].S)["alias"]
 }
 
